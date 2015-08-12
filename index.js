6a65276125dcefc294b1e49677516d705b5dcb62
Nitin Singh <nitin.singh2020@gmail.com>
2015-08-12 20:36:45 +0000

    Updates the code to read index.html

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express')
+var fs = require('fs');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Allison and Tao! Accept my e-Hi.');
+  //response.send('Hello Allison and Tao! Accept my e-Hi.');
+  fs.readFileSync('./index.html', function(err,data){
+    if (err) throw err;
+    response.send(data);
+  });
 })
 
 app.listen(app.get('port'), function() {
