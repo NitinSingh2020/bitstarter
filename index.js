a914324021caa707145f2eb0ce039f147d195a15
Nitin Singh <nitin.singh2020@gmail.com>
2015-08-12 03:08:25 +0000

    Changed to name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello New World!')
+  response.send('Hello Allison and Tao! Accept my e-Hi.');
 })
 
 app.listen(app.get('port'), function() {
